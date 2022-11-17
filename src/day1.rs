1abca167f17ea9fa73ce2f753123deac3bbb6fc1
ibrahim-akrab <ibrahim.m.akrab@gmail.com>
2022-11-17 09:38:12 +0200

    solve day1

---
+++ src/day1.rs
@@ -0,0 +1,31 @@
+use itertools::Itertools;
+use std::{fs, usize};
+
+pub fn day1a() -> String {
+    find_expense(2)
+}
+
+pub fn day1b() -> String {
+    find_expense(3)
+}
+
+fn find_expense(n: usize) -> String {
+    let values = read_data();
+    let result: Option<_> = values
+        .iter()
+        .combinations(n)
+        .find(|v| v.iter().copied().sum::<usize>() == 2020)
+        .map(|v| v.into_iter().product::<usize>());
+    match result {
+        Some(v) => v.to_string(),
+        None => "No solution".to_string(),
+    }
+}
+
+fn read_data() -> Vec<usize> {
+    let values = fs::read_to_string("inputs/day1").expect("Couldn't read file");
+    values
+        .split('\n')
+        .filter_map(|s| s.parse::<usize>().ok())
+        .collect()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use aoc2020::day1::{day1a, day1b};
+
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
     let problem = args.get(1).map(|s| s.as_str()).unwrap_or("None");
 
     let result = match problem {
-        _ => "Not yet solved",
+        "day1a" => day1a(),
+        "day1b" => day1b(),
+        _ => "Not yet solved".to_string(),
     };
     println!("{result}");
 }
