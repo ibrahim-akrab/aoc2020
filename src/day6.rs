029974d3ce1eae4471803a6a7f0be028fb4a3114
ibrahim-akrab <ibrahim.m.akrab@gmail.com>
2022-11-18 17:10:42 +0200

    remove unused code in day6 solution

--- src/day6.rs
+++ src/day6.rs
@@ -43,31 +43,6 @@ fn count_set_bits(mut bitmap: u32) -> u32 {
     count
 }
 
-// struct Group(u32);
-//
-// impl Group {
-//     fn new(s: &str) -> Self {
-//         // bitmap of each question answered with yes
-//         let value = s
-//             .split_whitespace()
-//             .map(|s| s.bytes())
-//             .flatten()
-//             .fold(0u32, |val, question| val | 1 << (question - b'a'));
-//         Self(value)
-//     }
-//
-//     fn count_questions(mut self) -> u32 {
-//         let mut count = 0;
-//         while self.0 != 0 {
-//             count += 1;
-//             self.0 &= self.0 - 1;
-//         }
-//         count
-//     }
-//
-//     fn count_and
-// }
-//
 fn read_data() -> Vec<String> {
     use std::fs;
     fs::read_to_string("inputs/day6.txt")
